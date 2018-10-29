286682a64e9896f5842a74253f15850cb6a79f7f
Beliriel <kke107152@iet-gibb.ch>
2018-10-29 15:47:58 +0100

    Update kikundi/model/interfaces/ProjectPoolImpl.php
    Added new Interfaces

---
+++ kikundi/model/interfaces/ProjectPoolImpl.php
@@ -0,0 +1,10 @@
+<?php
+    public function addProject();
+    public function deleteProject( $project );
+    public function getProjects( $project );
+    public function assignNewAdmin( $member );
+    public function getAdmin();
+    public function isMemberBySessionID( $sessionID );
+    public function getName();
+    public function analyzeProjects();
+?>
