ae8ec7c3442d209eaf2dbbf050a9d1abc7289c55
Beliriel <kke107152@iet-gibb.ch>
2018-11-05 13:48:42 +0100

    corrected model interfaces added MemberController-Interface
    see title

--- kikundi/model/interfaces/ProjectPoolImpl.php
+++ kikundi/model/interfaces/ProjectPoolImpl.php
@@ -1,10 +1,12 @@
 <?php
-    public function addProject();
-    public function deleteProject( $project );
-    public function getProjects( $project );
-    public function assignNewAdmin( $member );
-    public function getAdmin();
-    public function isMemberBySessionID( $sessionID );
-    public function getName();
-    public function analyzeProjects();
+    interface ProjectPoolImpl{
+        public function addProject();
+        public function deleteProject( $project );
+        public function getProjects( $project );
+        public function assignNewAdmin( $member );
+        public function getAdmin();
+        public function isMemberBySessionID( $sessionID );
+        public function getName();
+        public function analyzeProjects();
+    }
 ?>
