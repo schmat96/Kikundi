917333d3a8ae0428d4087e266a77d6b3c9da43ed
Mathias Schmid <mathias_schmid96@hotmail.com>
2018-11-05 13:43:01 +0100

    Update kikundi/ProjectController.php
    initial ProjectController commit

--- kikundi/ProjectController.php
+++ kikundi/ProjectController.php
@@ -1,9 +1,25 @@
 <?php
-class Foo {
-    public static function aStaticMethod() {
-        echo("asd");
+
+session_id(100);
+session_start();
+
+class ProjectController {
+
+
+	
+	public static function getAllPools() {
+		if (empty($_SESSION['allPools'])) {
+			$_SESSION['allPools'] = array();
+		}
+		return $_SESSION['allPools']; 
+	}
+	
+    public static function addProjectPool($sessid, $name, $adminName) {
+        array_push($_SESSION['allPools'], $sessid.":".$name.":".$adminName);
     }
 }
 
-Foo::aStaticMethod();
+var_dump(ProjectController::getAllPools());
+ProjectController::addProjectPool("Super", "Name", "Admin");
+var_dump(ProjectController::getAllPools());
 ?>
