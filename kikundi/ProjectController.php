ab5a20144669262d3021546f56e2aa0c2c9cf639
Beliriel <kke107152@iet-gibb.ch>
2018-11-19 11:23:06 +0100

    Refactor of Testing
    Eigener Test-Ordner, aber Tests werden immer noch im jeweiligen Klassenordner definiert. Wird evtl. noch geändert

--- kikundi/ProjectController.php
+++ kikundi/ProjectController.php
@@ -138,42 +138,7 @@ echo "<h1>Version 1.00 Beta</h1>";
 echo "There are no error validations or error feedbacks currently";
 
 if (isset($_GET['testing'])) {
-    if ($_GET['testing']=='reset') {
-        ProjectController::clearAllPools();
-    }
-    else if ($_GET['testing']=='all') {
-        $toTest = array();
-
-        require_once 'controller/TagControllerTest.php';
-        $tct = new TagControllerTest();
-        array_push($toTest, $tct);
-
-        foreach ($toTest as $test) {
-            if (count ($test->errors)===0) {
-                echo "<h1>Keine Errors in ".$test->getName()." gefunden.</h1>";
-            } else {
-                echo "<h1 style='background-color: red'>Folgende Errors wurden in ".$test->getName()." gefunden:</h1>";
-                var_dump($tct->errors);
-            }
-        }
-
-
-    } else {
-        echo "<h2>TESTING: All ADMIN HASHCODES</h2>";
-        ProjectController::getAllAdminHashCodes();
-        echo "<br>";
-        echo "<h2>TESTING: All MEMBER HASHCODES</h2>";
-        ProjectController::getAllMembersHashCodes();
-        echo "<h2>TESTING: All TAGS</h2>";
-        var_dump(TagController::getAllTags());
-        echo "<br>";
-        echo "<h2>TESTING: All Projects Likes</h2>";
-        var_dump(ProjectController::getAllProjectLikes());
-        echo "<br>";
-        echo "<h2>TESTING: IF YOU CAN INTERPRET THIS YOU ARE GODLIKE</h2>";
-        var_dump(ProjectController::getAllPools());
-    }
-
+    require_once 'tests/TestRunner.php';
 }
 
 ?>
